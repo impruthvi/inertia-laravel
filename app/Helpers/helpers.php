c132cc1c6bf4d4cadf83e32a95f6e243f2638353
impruthvi <pruthvirajput97@gmail.com>
2025-01-11 13:44:00 +0530

    Add get_ability helper function and enhance Controller with request authorization and validation traits

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,3 +6,18 @@ if (!function_exists('generatePassword')) {
         return \Hash::make($string);
     }
 }
+
+if (! function_exists('get_ability')) {
+    function get_ability($access): string
+    {
+        $routeNameArray = explode('.', request()->route()->getName());
+        switch (count($routeNameArray)) {
+            case '1':
+            case '2':
+                return $access . '_' . $routeNameArray[0];
+
+            case '3':
+                return $access . '_' . $routeNameArray[1];
+        }
+    }
+}
