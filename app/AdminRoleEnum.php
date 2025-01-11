e574f70db9edc3eb63c0bb4aed5d673699f1dc1e
impruthvi <pruthvirajput97@gmail.com>
2025-01-11 15:11:44 +0530

    Add AdminRoleEnum and enhance admin permissions management with new helper functions and migrations

---
+++ app/AdminRoleEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+enum AdminRoleEnum: string
+{
+    case ADMIN = 'admin';
+}
--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,28 @@
 <?php
 
+use Spatie\Permission\Models\Permission;
+
+if (! function_exists('admin_roles')) {
+    function admin_roles(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'name' => 'Users',
+                'permissions' => ['add', 'edit', 'view', 'delete'],
+                'route_prefix' => 'users',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Roles',
+                'permissions' => ['add', 'edit', 'view', 'delete'],
+                'route_prefix' => 'roles',
+            ],
+
+        ];
+    }
+}
+
 if (!function_exists('generatePassword')) {
     function generatePassword(string $string = ''): string
     {
@@ -18,6 +41,41 @@ if (! function_exists('get_ability')) {
 
             case '3':
                 return $access . '_' . $routeNameArray[1];
+        }
+    }
+}
+
+if (! function_exists('get_system_permissions')) {
+    function get_system_permissions(array $permissions): array
+    {
+        $permissions_array = [];
+
+        foreach ($permissions as $permission) {
+            array_push($permissions_array, 'access_' . $permission['route_prefix']);
+            foreach ($permission['permissions'] as $access) {
+                array_push($permissions_array, $access . '_' . $permission['route_prefix']);
+            }
+        }
+
+        return $permissions_array;
+    }
+}
+
+if (! function_exists('role_permissions')) {
+    function role_permissions($role = 'admin'): array
+    {
+        if ($role === 'admin') {
+            return admin_roles();
+        }
+    }
+}
+
+if (! function_exists('create_permissions')) {
+    function create_permissions(array $defaultPermissions, $role, string $guard_name = 'admin'): void
+    {
+        foreach ($defaultPermissions as $defaultPermission) {
+            $permission = Permission::updateOrCreate(['name' => $defaultPermission, 'guard_name' => $guard_name]);
+            $role->givePermissionTo($permission);
         }
     }
 }
