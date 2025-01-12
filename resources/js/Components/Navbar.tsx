4e06bfa16ad914cecdc088b03b9ba8822bb7e081
impruthvi <pruthvirajput97@gmail.com>
2025-01-12 11:57:48 +0530

    Add action descriptions for user and role management in Navbar and update route handling in Navigation

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -5,6 +5,30 @@ const pathnameMap = {
     users: {
         title: "Users",
         description: "View all users here",
+        actions: {
+            create: {
+                title: "Create User",
+                description: "Add a new user here",
+            },
+            edit: {
+                title: "Edit User",
+                description: "Modify user details here",
+            },
+        },
+    },
+    roles: {
+        title: "Roles",
+        description: "View all roles here",
+        actions: {
+            create: {
+                title: "Create Role",
+                description: "Add a new role here",
+            },
+            edit: {
+                title: "Edit Role",
+                description: "Modify role details here",
+            },
+        },
     },
 };
 
@@ -17,9 +41,20 @@ export const Navbar = () => {
     const pathname = route().current() as string;
 
     const pathnameParts = pathname.split(".");
-    const pathnameKey = pathnameParts[1] as keyof typeof pathnameMap;
+    const mainKey = pathnameParts[1] as keyof typeof pathnameMap;
+    const actionKey =
+        pathnameParts[2] as keyof (typeof pathnameMap)["users"]["actions"];
 
-    const { description, title } = pathnameMap[pathnameKey] || defaultMap;
+    const defaultActionMap = { title: "", description: "" };
+    const actionMap =
+        pathnameMap[mainKey]?.actions?.[actionKey] || defaultActionMap;
+
+    const title =
+        actionMap.title || pathnameMap[mainKey]?.title || defaultMap.title;
+    const description =
+        actionMap.description ||
+        pathnameMap[mainKey]?.description ||
+        defaultMap.description;
 
     return (
         <nav className="pt-4 px-6 flex items-center justify-between">
