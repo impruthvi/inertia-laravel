bf7695036a6074f712ab80b82adf796f359b2f02
impruthvi <pruthvirajput97@gmail.com>
2025-01-13 11:58:43 +0530

    Add is_common_role field and created_by/updated_by foreign keys to permissions table

--- database/migrations/2025_01_11_075923_create_permission_tables.php
+++ database/migrations/2025_01_11_075923_create_permission_tables.php
@@ -46,6 +46,15 @@ return new class extends Migration
 
             // custom fields
             $table->string('display_name')->nullable();
+            $table->boolean('is_common_role')->nullable();
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('admins')
+                ->onDelete('SET NULL');
+            $table->foreignId('updated_by')
+                ->nullable()
+                ->constrained('admins')
+                ->onDelete('SET NULL');
 
             $table->timestamps();
             if ($teams || config('permission.testing')) {
@@ -70,13 +79,16 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                $table->primary(
+                    [$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary'
+                );
             } else {
-                $table->primary([$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                $table->primary(
+                    [$pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_permissions_permission_model_type_primary'
+                );
             }
-
         });
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole, $teams) {
@@ -94,11 +106,15 @@ return new class extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key']);
                 $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
 
-                $table->primary([$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+                $table->primary(
+                    [$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary'
+                );
             } else {
-                $table->primary([$pivotRole, $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_roles_role_model_type_primary');
+                $table->primary(
+                    [$pivotRole, $columnNames['model_morph_key'], 'model_type'],
+                    'model_has_roles_role_model_type_primary'
+                );
             }
         });
 
