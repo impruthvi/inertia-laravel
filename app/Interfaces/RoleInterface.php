684a6668700be7d5a831bec37726b7086017f05a
impruthvi <pruthvirajput97@gmail.com>
2025-01-15 10:03:38 +0530

    Add find, update, and delete methods to RoleInterface and RoleRepository for enhanced role management

--- app/Interfaces/RoleInterface.php
+++ app/Interfaces/RoleInterface.php
@@ -8,4 +8,10 @@ interface RoleInterface
 {
 
     public function store(array $attributes): Role;
+
+    public function find(string $id): ?Role;
+
+    public function update(int $id, array $attributes): bool;
+
+    public function delete(int $id): bool;
 }
--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -22,4 +22,29 @@ class RoleRepository implements RoleInterface
 
         return $role;
     }
+
+    public function find(string $id): ?Role
+    {
+        // return Role::with(['favorite', 'permissions'])->visibility()->withTrashed()->find($id);
+        return Role::find($id);
+    }
+
+    public function update(int $id, array $attributes): bool
+    {
+        $role = $this->find($id);
+
+        $role->syncPermissions($attributes['permissions']);
+
+        // if (auth()->user()->role == AdminRoleEnum::PRACTICE->value || auth()->user()->role == AdminRoleEnum::PROVIDER->value) {
+        //     return true;
+        // }
+
+        return $role->update(['display_name' => $attributes['display_name']]) > 0;
+    }
+
+    public function delete(int $id): bool
+    {
+        // return Role::withTrashed()->findOrFail($id)->delete() > 0;
+        return Role::findOrFail($id)->delete() > 0;
+    }
 }
