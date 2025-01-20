b6afcfe5237272a7b7a5b3f6353e5105d29b7e5d
impruthvi <pruthvirajput97@gmail.com>
2025-01-20 14:29:43 +0530

    chore: add UpdateUserRequest for user validation and enforce strict return types in UserController

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\CreateUserRequest;
+use App\Http\Requests\Admin\User\UpdateUserRequest;
 use App\Interfaces\UserInterface;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -51,7 +54,7 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): void
     {
         //
     }
@@ -59,7 +62,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(CreateUserRequest $request)
+    public function store(CreateUserRequest $request): RedirectResponse
     {
         $this->authorize(get_ability('add'));
 
@@ -76,7 +79,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): void
     {
         //
     }
@@ -84,7 +87,7 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): JsonResponse
     {
         // Get user
         $user = $this->userInterface->find($id);
@@ -97,12 +100,13 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): \Illuminate\Http\RedirectResponse
+    public function update(UpdateUserRequest $request, string $id):RedirectResponse
     {
         $user = $this->userInterface->find($id);
+
         $this->authorize(get_ability('edit'), $user);
 
-        $this->userInterface->update($id, $request->all());
+        $this->userInterface->update($id, $request->validated());
 
         return redirect()->back()->with('success', 'User Updated successfully');
     }
@@ -110,7 +114,7 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
         $this->authorize(get_ability('delete'));
         $this->userInterface->delete($id);
