7f84996cc300406fc679a8c264e2adc6c52eb69d
impruthvi <pruthvirajput97@gmail.com>
2025-01-22 18:43:43 +0530

    refactor: remove RegisteredAdminController and update routes accordingly

--- routes/admin/auth.php
+++ routes/admin/auth.php
@@ -3,18 +3,12 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController as AuthAuthenticatedSessionController;
-use App\Http\Controllers\Admin\Auth\RegisteredAdminController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::prefix('admin')->middleware('guest:admin')->as('admin.')->group(function () {
-
-    Route::get('register', [RegisteredAdminController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredAdminController::class, 'store']);
-
     Route::get('login', [AuthAuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthAuthenticatedSessionController::class, 'store'])->name('login');
-
 });
 
 Route::prefix('admin')->middleware('auth:admin')->as('admin.')->group(function () {
