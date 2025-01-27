2dfcdfcf8846873d6278c0d24e845ef905c9df83
impruthvi <pruthvirajput97@gmail.com>
2025-01-27 19:02:29 +0530

    format: lint the file

--- tests/Unit/Repositories/UserRepositoryTest.php
+++ tests/Unit/Repositories/UserRepositoryTest.php
@@ -1,10 +1,10 @@
 <?php
+
 // i just write missing coverage test cases for UserRepository
 declare(strict_types=1);
 
-
-use App\Models\User;
 use App\Interfaces\UserInterface;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
